5d9a261d0a3d359b25904df1ff61f3549955b78e
bhavinparmar005 <bhavinparmar005@gmail.com>
2026-05-27 15:20:39 +0530

    SETUP APP

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_home_page_returns_200(self):
+        response = self.client.get(reverse('homePage'))
+
+        self.assertEqual(response.status_code, 200)
